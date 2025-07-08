bd5d7c5dc333732bf4804f2319441dd6445bd7be
BraisonWabwire <braisonwabwire2003@gmail.com>
2025-07-08 20:43:24 +0300

    working with container and images

--- lib/main.dart
+++ lib/main.dart
@@ -12,43 +12,50 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Color.fromRGBO(22, 22, 41, 0.632),
         appBar: AppBar(
-          backgroundColor:Color.fromRGBO(22, 22, 41, 0.632),
-          title: const Text("Welcome to winx player", 
-          style: TextStyle(color: Color.fromARGB(211, 255, 255, 255),fontWeight:FontWeight.bold,),
+          backgroundColor: Color.fromRGBO(22, 22, 41, 0.632),
+          title: const Text(
+            "Welcome to winx player",
+            style: TextStyle(
+              color: Color.fromARGB(211, 255, 255, 255),
+              fontWeight: FontWeight.bold,
+            ),
           ),
           centerTitle: true,
-
         ),
-        body:Column(
+        body: Column(
           children: [
             Expanded(
               child: Container(
                 decoration: BoxDecoration(
-                image: DecorationImage(image: AssetImage('assets/images/home_image.jpg'),
-                fit: BoxFit.cover,
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/home_image.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                  color: Color.fromRGBO(11, 11, 20, 0.843),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                  ),
                 ),
-                color: Color.fromRGBO(11, 11, 20, 0.843),
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(40),
-                  topRight: Radius.circular(40),
-                )
-                ),
-                
+
                 alignment: Alignment.topCenter,
                 padding: EdgeInsets.all(30),
-                child:const Text("Hi, enjoy your favorite music", style: TextStyle(color: Color.fromARGB(255, 5, 5, 5),fontSize: 15)),
+                child: const Text(
+                  "Hi, enjoy your favorite music",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 5, 5, 5),
+                    fontSize: 15,
+                  ),
+                ),
               ),
             ),
           ],
-        ) ,
+        ),
         floatingActionButton: FloatingActionButton(
-          child: Icon(
-            Icons.headphones,
-          ),
+          child: Icon(Icons.headphones),
           onPressed: () => {},
         ),
       ),
-
     );
   }
 }
